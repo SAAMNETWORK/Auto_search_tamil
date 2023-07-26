39e8e77c9f5c59b77056e657a6cad00681c8be33
SAAMNETWORK <140245198+SAAMNETWORK@users.noreply.github.com>
2023-07-26 15:16:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_photo("https://telegra.ph/file/b806ad314d0c415571bde.jpg") 
+        m=await message.reply_photo("https://telegra.ph/Png-saam-Network-07-24") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
